45f68be134ba12490925e21cbbd3f7f851329b07
Devin <devincb93@gmail.com>
2025-05-01 22:12:40 -0700

    Fixed small sizing issue in video div of Hero video, and also fixed text in Video carousel to fit the size of the videos more

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,7 +28,7 @@ const Hero = () => {
     <section className='w-full nav-height bg-black relative'>
         <div className='h-5/6 w-full flex-center flex-col'>
         <p id="hero" className='hero-title'>Iphone 15 Pro</p>
-        <div className='md:w-10/12 w-9/12'>
+        <div className='sm:w-11/12 md:w-10/12 w-9/12'>
         <video autoPlay muted playsInline={true} key={videoSrc}>
             <source src={videoSrc} type='video/mp4'/>
         </video>
