d00e17e5d2e025047121ab02db1268a5e0654231
Devin <devincb93@gmail.com>
2025-05-07 01:57:51 -0700

    added toggle button for size small and large for model views

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -7,7 +7,7 @@ import { Canvas } from '@react-three/fiber'
 
 import * as Three from 'three'
 import { View } from '@react-three/drei'
-import { models } from '../constants'
+import { models, sizes } from '../constants'
 
 const Model = () => {
 
@@ -72,9 +72,14 @@ const Model = () => {
               <div>
                 <ul className='color-container'>
                   {models.map((item, i)=> (
-                    <li key={i} className='w-6 h-6 rounded-full mx-2 ' style={{backgroundColor: item.color[0]}} onClick={() => {setModel(item)}}/>
+                    <li key={i} className='w-6 h-6 rounded-full mx-2 cursor-pointer ' style={{backgroundColor: item.color[0]}} onClick={() => {setModel(item)}}/>
                   ))}
                 </ul>
+                <button className='size-btn-container'>
+                  {sizes.map(({label, value})=> (
+                    <span key={label} className='size-btn' style={{backgroundColor: size === value ? 'white' : 'transparent', color: size === value ? 'black' : ' white'}} onClick={()=> setSize(value)}>{label}</span>
+                  ))}
+                </button>
               </div>
             </p>
           </div>
