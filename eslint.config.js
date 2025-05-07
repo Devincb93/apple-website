cb715c6b7129bb985b4f6d4456e87c051af36531
Devin <devincb93@gmail.com>
2025-05-07 15:08:53 -0700

    added iphone model for 360 viewing, not fised in position yet

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import { ReactThreeFiber } from '@react-three/fiber'
 
 export default [
   { ignores: ['dist'] },
@@ -18,7 +19,7 @@ export default [
     },
     plugins: {
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      'react-refresh': reactRefresh
     },
     rules: {
       ...js.configs.recommended.rules,
